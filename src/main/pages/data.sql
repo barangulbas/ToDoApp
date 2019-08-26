5ab723a0ce2bfc9a80789f5a528fed3cb5759eb1
barangulbas <baran@uncosoft.com>
2019-08-26 09:44:48 +0300

    insert into table

--- src/main/pages/data.sql
+++
@@ -1,9 +0,0 @@
-insert into tasks values (1,false,'2010-01-04','work',3,'Add a create button');
-insert into tasks values (2,false,'2010-12-04','work',2,'Add a new task adding page');
-insert into tasks values (5,false,'2010-07-04','motocycle',3,'Clean the chain');
-insert into tasks values (6,false,'2010-01-06','home',3,'Collect laundries');
-insert into tasks values (4,true,'2010-01-04','motocycle',1,'Diablo needs fuel!!!');
-insert into tasks values (3,false,'2010-01-22','work',4,'Make that checkbox as soon as possible');
-insert into tasks values (21,false,'2010-01-05','home',3,'wash the dishes');
-
-INSERT INTO roles(name) VALUES('ROLE_USER');
