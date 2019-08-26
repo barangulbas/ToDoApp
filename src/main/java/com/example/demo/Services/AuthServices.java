c8e63ea6f18aa7ff904746d3a2fe905555dc4484
barangulbas <baran@uncosoft.com>
2019-08-26 09:54:55 +0300

    add AuthServices

---
+++ src/main/java/com/example/demo/Services/AuthServices.java
@@ -0,0 +1,89 @@
+package com.example.demo.Services;
+
+import com.example.demo.Repositories.RoleRepository;
+import com.example.demo.Repositories.UserRepository;
+import com.example.demo.exception.AppException;
+import com.example.demo.model.Role;
+import com.example.demo.model.RoleName;
+import com.example.demo.model.User;
+import com.example.demo.payload.ApiResponse;
+import com.example.demo.payload.JwtAuthenticationResponse;
+import com.example.demo.payload.LoginRequest;
+import com.example.demo.payload.SignUpRequest;
+import com.example.demo.security.JwtTokenProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collections;
+
+@Service
+public class AuthServices {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    JwtTokenProvider tokenProvider;
+
+    public ResponseEntity<?> authenticateUser(LoginRequest loginRequest){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsernameOrEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.generateToken(authentication);
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    public ResponseEntity<?> registerUser(SignUpRequest signUpRequest){
+        if(userRepository.existsByUsername(signUpRequest.getUsername())){
+            return new ResponseEntity<>(new ApiResponse(false, "username is already taken!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if(userRepository.existsByEmail(signUpRequest.getEmail())){
+            return new ResponseEntity<>(new ApiResponse(false, "Email Address already in use!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        //creating user's account
+        User user = new User(signUpRequest.getName(), signUpRequest.getUsername(),
+                signUpRequest.getEmail(), signUpRequest.getPassword());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new AppException("User Role not set."));
+
+        user.setRoles(Collections.singleton(userRole));
+
+        User result = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/api/users/{username}")
+                .buildAndExpand(result.getUsername()).toUri();
+
+        return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
+
+    }
+
+
+}
